c9108391476fa9b4eb8990db80dbf586a5b344d3
luckyboy07 <cairudin.16@gmail.com>
2024-10-01 11:29:18 +0800

    modal implementation

--- src/index.js
+++ src/index.js
@@ -6,3 +6,40 @@ document.querySelectorAll('.card a').forEach(link => {
         alert('This feature is not available yet!');
     });
 }); 
+
+
+const body = document.body;
+const links = document.querySelectorAll('a');
+const modalOverlay = document.getElementsByClassName('modal-overlay')[0];
+const modalImg = document.querySelector('#modal-image');
+
+links.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('link--', link);
+
+    if (link.classList.contains('block-img')) {
+      body.style.overflow = 'hidden';
+      modalOverlay.classList.add('show');
+
+      const imgSrc = link.querySelector('img').src;
+      modalImg.src = imgSrc;
+    }
+
+    return true;
+  });
+});
+
+modalOverlay.addEventListener('click', () => {
+  modalOverlay.classList.remove('show');
+  body.style.overflow = 'auto';
+
+  return true;
+});
+
+// Prevent bubbling up the DOM - will not close modal
+modalImg.addEventListener('click', (e) => {
+  e.stopPropagation();
+
+  return true;
+});
